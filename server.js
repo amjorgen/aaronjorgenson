e357dff9e27e12e9fd28a6edc92ae1c36f4a447b
Aaron Jorgenson <amjorgen1988@gmail.com>
2014-07-12 14:41:54 -0700

    general changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public/css', express.static(path.join(__dirname, '/public/css')));
 app.get('/', function(req, res){
-  res.sendfile('./resume.html');
+  res.sendfile('./public/views/resume.html');
 });
 
 app.listen(3000);

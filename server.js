74d28902f8085bd7fd688e21a80b42ea2aa68bee
Michael Ross <michael.stewart.ross@gmail.com>
2014-08-24 11:25:16 -0700

    Contact posting

--- server.js
+++ server.js
@@ -4,30 +4,16 @@ var app = express();
 var mongoose = require('mongoose');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
-// Database
 
-// In mongo shell create database called aaronjorgenson
-// then add a record to the contacts collection in that db
-// browse to localhost:3000/api/contacts and you should see
-// the record.
+// Database
 mongoose.connect('mongodb://localhost/aaronjorgenson');
 
 //Configuration
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public')));
 
-// jQuery.post("/api/contacts", {
-//   "name": "Aaron",  
-//   "company": "Tits",  
-//   "email": "test@test.com",
-//   "comment": "woohoo"
-// }, function (data, textStatus, jqXHR) { 
-//     console.log("Post resposne:"); console.dir(data); console.log(textStatus); console.dir(jqXHR); 
-// });
-
 //Schema
-
 var Schema = mongoose.Schema;
 var Contact = new Schema({
   name: {type: String, required: true},
@@ -38,7 +24,6 @@ var Contact = new Schema({
 var ContactModel = mongoose.model('Contact', Contact);
 
 // Routes
-
 app.use('/public/css', express.static(path.join(__dirname, '/public/css')));
 app.use('/public/js', express.static(path.join(__dirname, '/public/js')));
 app.use('/public/images', express.static(path.join(__dirname, '/public/images')));
@@ -57,8 +42,6 @@ app.get('/api/contacts', function(req, res) {
 
 app.post('/api/contacts', function (req, res){
   var contact;
-  console.log("POST: ");
-  console.log(req.body);
   contact = new ContactModel({
     name: req.body.name,
     company: req.body.company,
@@ -74,7 +57,6 @@ app.post('/api/contacts', function (req, res){
   });
   return res.send(contact);
 });
-
 
 app.listen(3000);
 console.log("You are listening on localhost:3000");
